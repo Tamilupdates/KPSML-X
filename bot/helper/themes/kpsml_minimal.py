c6d13950db221c2ae87a1189e1eda57663173d36
Nandhakumar K <116391685+Tamilupdates@users.noreply.github.com>
2024-02-09 14:08:52 +0530

    latest

--- bot/helper/themes/kpsml_minimal.py
+++ bot/helper/themes/kpsml_minimal.py
@@ -6,8 +6,8 @@ class KPSMLStyle:
     ST_BN1_URL = 'https://telegram.me/KPSBots'
     ST_BN2_NAME = 'Updates 🔥'
     ST_BN2_URL = 'https://telegram.me/KPSTorrent'
-    ST_MSG = '''<i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>\n
-                <b>Type {help_command} to get a list of available commands</b>'''
+    ST_MSG = '''<b><i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>\n
+Type {help_command} to get a list of available commands</b>'''
     ST_BOTPM = '''<i>Now, This bot will send all your files and links here. Start Using ...</i>'''
     ST_UNAUTH = '''<i>You Are not authorized user! Deploy your own KPSML-X Mirror-Leech bot</i>'''
     OWN_TOKEN_GENERATE = '''<b>Temporary Token is not yours!</b>\n\n<i>Kindly generate your own.</i>'''
