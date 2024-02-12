9cacc91162d41d33ccb3204d9b147258f8da2ad6
Nandhakumar K <116391685+Tamilupdates@users.noreply.github.com>
2024-02-12 11:37:14 +0530

    Update update.py

--- update.py
+++ update.py
@@ -52,11 +52,11 @@ if DATABASE_URL is not None:
 
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
 if len(UPSTREAM_REPO) == 0:
-    UPSTREAM_REPO = "https://github.com/Tamilupdates/KPSML-X"
+    UPSTREAM_REPO = ""
 
 UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
 if len(UPSTREAM_BRANCH) == 0:
-    UPSTREAM_BRANCH = 'hk_kpsmlx'
+    UPSTREAM_BRANCH = 'master'
 
 if UPSTREAM_REPO is not None:
     if ospath.exists('.git'):
