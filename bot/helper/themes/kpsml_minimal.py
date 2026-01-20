0a19f04b84afdd1de21b1f21540c15b428b7a729
maran117 <153261389+maran117@users.noreply.github.com>
2026-01-20 23:30:36 +0530

    Update kpsml_minimal.py

--- bot/helper/themes/kpsml_minimal.py
+++ bot/helper/themes/kpsml_minimal.py
@@ -177,8 +177,8 @@ Type {help_command} to get a list of available commands</b>'''
 
     #####---------PROGRESSIVE STATUS-------
     BAR =               '\n┃ {Bar}'
-    PROCESSED =         '\n┠ <b>Processed:</b> {Processed}'
-    STATUS =            '\n┠ <b>Status:</b> <a href="{Url}">{Status}</a>'
+    🔥PROCESSED =         '\n┠ <b>Processed:</b> {Processed}'
+    🎉STATUS =            '\n┠ <b>Status:</b> <a href="{Url}">{Status}</a>'
     ETA =                                                ' | <b>ETA:</b> {Eta}'
     SPEED =             '\n┠ <b>Speed:</b> {Speed}'
     ELAPSED =                                     ' | <b>Elapsed:</b> {Elapsed}'
